89e58a9bdd811fbc1f88a47f743e412d34a537d7
M K <thorncmr@gmail.com>
2021-09-21 20:44:33 +1000

    moving to rollup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+  // Common js and ES module builds
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/base-js-tools.js',
+      format: 'umd',
+      name: "baseJsTools"
+    },
+    plugins: [typescript()]
+  }
+]
